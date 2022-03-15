69aa73d0deea45ae8b763f64648814c494cb55c0
Zsolt <5047426+bodzaital@users.noreply.github.com>
2022-03-15 19:50:48 +0100

    yeet

--- btng/btif-source/scenes/__sceneName/__sceneName.js
+++
@@ -1,3 +0,0 @@
-import { $, $$, LoadFile, ResolveSceneByName, ResolveThemeByName, NullConditional, CreateElementByDescriptor, NullishCoalescingOp } from "../../modules/utils.js";
-
-// Put scene-only code here, runs when the scene is loaded.
--- btng/btif-source/scenes/scene-1/scene-1.js
+++
@@ -1,5 +0,0 @@
-import { $ } from "../../modules/utils.js";
-
-$("#name").innerText = data.Get("player");
-
-data.Subscribe($("#name"), "innerText", "player");
